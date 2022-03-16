68b0a236771d92a3e35b5885a4ed6e1c9578a879
iwmwargin <95541348+iwmwargin@users.noreply.github.com>
2022-03-16 19:18:46 -0400

    done more

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+const mysql = require("mysql2");
 const express = require('express');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "password",
+        database: "election"
+    },
+    console.log("Connected to the election database.")
+);
 
 
 
@@ -13,11 +24,9 @@ app.use(express.json());
 
 
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World!"
-    });
-});
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+  });
 
 app.use((req, res) => {
     res.status(404).end();
